18d50d06dc4e66d313e0ff2b71988b09a2666a46
Soumya Ranjan Barik <soumya.vedalytics@gmail.com>
2024-05-03 13:11:26 +0530

    added global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-import express from "express"
+import express, { NextFunction, Request, Response } from "express"
 import cors from "cors"
 import morgan from "morgan"
 import { config } from "./config/config"
-import connectDB from "./config/dbConnection"
+import connectDB from "./config/dbConnection" 
+import globalErrorHandler from "./middlewares/globalErrorHandler"
 
 const app = express()
 const port = config.port || 8086
@@ -13,8 +14,13 @@ app.use(morgan("dev"));
 // DB Connection
 connectDB()
 
+app.get("/", (req: Request, res: Response) => {
+     
+})
 
 
+// Global error handler
+app.use(globalErrorHandler)
 
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`)
--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,8 @@ conf()
 
 const _config = {
     port: process.env.PORT,
-    mongodb_url:process.env.MONGO_CONNECTION_STRIG
+    mongodb_url:process.env.MONGO_CONNECTION_STRIG,
+    env:process.env.Node_ENV
 };
 
 export const config = Object.freeze(_config);
