9164c473793d5dcc67b3eb74b28d23a36fbf9eeb
Soumya Ranjan Barik <soumya.vedalytics@gmail.com>
2024-06-03 18:48:02 +0530

    add update book functionality

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,6 +1,6 @@
 import path from "node:path"
 import express from "express"
-import { createBook } from "./bookController"
+import { createBook, updateBook } from "./bookController"
 import multer from "multer"
 import authenticate from "../middlewares/authenticate"
 
@@ -8,7 +8,7 @@ const bookRouter = express.Router()
 
 const upload = multer({
     dest: path.resolve(__dirname, '../../public/data/uploads'),
-    limits: { fieldSize: 3e7 }//30mb
+    limits: { fieldSize: 10000 }//10 mb
 })
 
 bookRouter
@@ -18,5 +18,11 @@ bookRouter
             { name: 'coverImage', maxCount: 1 },
             { name: 'file', maxCount: 1 }
         ]), createBook)
+    .patch("/:bookId",
+        authenticate,
+        upload.fields([
+            { name: 'coverImage', maxCount: 1 },
+            { name: 'file', maxCount: 1 }
+        ]), updateBook)
 
 export default bookRouter
